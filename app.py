46e01cf80c8b182b0d84d4f609bd347c7084a290
Vishal Limgire <vrl@narola.email>
2021-05-26 19:41:47 +0530

    combined model

--- app.py
+++ app.py
@@ -6,16 +6,37 @@ import numpy as np
 
 app = Flask(__name__)
 
-classes = {
-    'Carlsberg':[0., 0., 1., 0., 0., 0., 0., 0.] , 
-    'Tuborg': [0., 0., 0., 0., 0., 0., 1., 0.], 
-    '1664': [1., 0., 0., 0., 0., 0., 0., 0.], 
-    'Brooklyn': [0., 1., 0., 0., 0., 0., 0., 0.] , 
-    'Kongens': [0., 0., 0., 0., 1., 0., 0., 0.], 
-    'Wiibroe': [0., 0., 0., 0., 0., 0., 0., 1.] ,
-    'Jacobsen':[0., 0., 0., 1., 0., 0., 0., 0.] , 
-    'Kronenbourg': [0., 0., 0., 0., 0., 1., 0., 0.]
+# classes = {
+#     'Carlsberg':[0., 0., 1., 0., 0., 0., 0., 0.] , 
+#     'Tuborg': [0., 0., 0., 0., 0., 0., 1., 0.], 
+#     '1664': [1., 0., 0., 0., 0., 0., 0., 0.], 
+#     'Brooklyn': [0., 1., 0., 0., 0., 0., 0., 0.] , 
+#     'Kongens': [0., 0., 0., 0., 1., 0., 0., 0.], 
+#     'Wiibroe': [0., 0., 0., 0., 0., 0., 0., 1.] ,
+#     'Jacobsen':[0., 0., 0., 1., 0., 0., 0., 0.] , 
+#     'Kronenbourg': [0., 0., 0., 0., 0., 1., 0., 0.]
+# }
+
+brand_type = {
+        'Læsk':[0., 1., 0.],
+         'Cider':[1., 0., 0.],
+          'Øl':[0., 0., 1.]
 }
+
+energy_type = {
+        'CO2':[1., 0., 0., 0.], 
+        'Electricity':[0., 1., 0., 0.],
+        'Water':[0., 0., 0., 1.],
+        'Heat':[0., 0., 1., 0.]
+}
+
+       
+       
+       
+       
+       
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def image_match():
@@ -23,9 +44,11 @@ def image_match():
         resp = request.get_json().get('data')
         # selected_date = datetime.strptime(resp.get('selectedDate'), '%Y-%m-%d').date()
         # selected_date = selected_date.toordinal()
-        selected_brand = classes.get(resp.get('brand_name'))
-        loaded_model = pickle.load(open('O2_Heat_Beer_data.pkl', 'rb'))
-        data = np.append(selected_brand, [resp.get('volumnHLT')])
+        selected_brand = brand_type.get(resp.get('brand_type'))
+        selected_energy = energy_type.get(resp.get('energy_type'))
+        loaded_model = pickle.load(open('combine_model.pkl', 'rb'))
+        data = np.append(selected_brand, selected_energy)
+        data =   np.append(data, [resp.get('volumnHLT')])
         # pred = np.array([[0., 0., 1., 0., 0., 0., 0., 0.,737073, 506.088]])
         pred = np.array([data])
         result = loaded_model.predict(pred)
